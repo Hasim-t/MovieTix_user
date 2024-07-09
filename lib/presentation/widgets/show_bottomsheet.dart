9031a66a07cde424ac70ac6b9ab9cc7aff5da0e4
Hashim <hashimmuhammad838@gmail.com>
2024-07-09 19:31:14 +0530

    profile

--- lib/presentation/widgets/show_bottomsheet.dart
+++ lib/presentation/widgets/show_bottomsheet.dart
@@ -2,40 +2,68 @@
 
 import 'package:flutter/material.dart';
 import 'package:movie/presentation/constants/color.dart';
+import 'package:movie/presentation/widgets/textfromwidget.dart';
 
 void showProfileBottomSheet(BuildContext context) {
   double screenHeight = MediaQuery.of(context).size.height;
   double screenWidth = MediaQuery.of(context).size.width;
-  
+
+  TextEditingController namecontroller = TextEditingController();
+  TextEditingController  phonecontroller = TextEditingController();
+  TextEditingController  Gentercontroller  = TextEditingController();
+  TextEditingController   Dobcontroller  = TextEditingController();
+  TextEditingController   maragecontroller  = TextEditingController();
+
+
   showModalBottomSheet(
     context: context,
     backgroundColor: Colors.transparent,
-    barrierColor: Colors.white.withOpacity(0.1), 
+    barrierColor: Colors.white.withOpacity(0.1),
     builder: (BuildContext context) {
-      return Container(
-        height: screenHeight * 0.5,
-        width: screenWidth,
-        decoration: BoxDecoration(
-          color: MyColor().darkblue,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
-          ),
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            SizedBox(height: 15),
-            Container(
-              width: 50,
-              height: 5,
-              decoration: BoxDecoration(
-                color: MyColor().primarycolor,
-                borderRadius: BorderRadius.circular(30),
-              ),
+      return SingleChildScrollView(
+        child: Container(
+          
+          width: screenWidth,
+          decoration: BoxDecoration(
+            color: MyColor().darkblue,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
             ),
-            // Add more content for your bottom sheet here
-          ],
+          ),
+          child: Padding(
+             padding:  const EdgeInsets.symmetric(horizontal:30),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                const SizedBox(height: 15),
+                Container(
+                  width: 50,
+                  height: 5,
+                  decoration: BoxDecoration(
+                    color: MyColor().primarycolor,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                SizedBox(
+                  height:  10,
+                ),
+                Text("Edit Profile",style: TextStyle(color: MyColor().primarycolor),),
+                const SizedBox(height: 20,),
+                CustomTextFormField(controller: namecontroller, hintText: "Name"),
+               const  SizedBox(height: 20,),
+                CustomTextFormField(controller: phonecontroller, hintText: "Phone"),
+           const        SizedBox(height: 20,),
+                CustomTextFormField(controller: Gentercontroller, hintText: "Genter"),
+              const  SizedBox(height: 20,),
+               CustomTextFormField(controller: Dobcontroller, hintText: "Date of Birth"),
+               const  SizedBox(height: 20,),
+                CustomTextFormField(controller: maragecontroller, hintText: "Marital Status"),
+              const   SizedBox(height: 20,),
+              
+              ],
+            ),
+          ),
         ),
       );
     },
